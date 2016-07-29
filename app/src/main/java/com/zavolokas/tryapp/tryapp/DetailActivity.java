d818f9bbd59393a1675884e6405b4bea8d02fd08
zavolokas <sergey.zavoloka@gmail.com>
2016-07-29 19:16:46 +0200

    added new DetailActivity

---
+++ app/src/main/java/com/zavolokas/tryapp/tryapp/DetailActivity.java
@@ -0,0 +1,32 @@
+package com.zavolokas.tryapp.tryapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        if (savedInstanceState!=null){
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DetailActivityFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return true;
+    }
+
+
+}
